da0eadcc8fd8305c985b05aba7e1bdd8a5578bde
Mudassir Mirza <hello.mudassirmirza@gmail.com>
2024-07-04 03:26:58 +0500

    run alembic  migrations

--- be/venv/models.py
+++ be/venv/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Enum, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from .database import Base
+from venv.database import Base
 import enum
 
 class RoleEnum(enum.Enum):
